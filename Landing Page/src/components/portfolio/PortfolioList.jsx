770927c3e97b085b84ba058bb6cdc61ca3bfbf28
Akintunde Kayode <kayodeakintunde919@gmail.com>
2025-08-10 13:39:36 +0100

    New update on portfolio

--- Landing Page/src/components/portfolio/PortfolioList.jsx
+++ Landing Page/src/components/portfolio/PortfolioList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import image from "../../assets/herobg.jpg";
+import digit from "../../assets/digital.png";
+import image_1 from "../../assets/finance.png";
 import Contact from "../../components/Contact";
 
 const PortfolioList = () => {
@@ -21,98 +23,18 @@ const PortfolioList = () => {
       </div>
 
       {/*  */}
-      {/*  */}
-      {/*  */}
-      {/*  */}
-
-      <div className="md:flex gap-10  items-center justify-between px-[8%] mt-[100px] mb-10">
-        <div className="relative cursor-pointer port">
-          <img src={image} alt="" className="" />
-          <div className="content absolute top-0 bottom-0 right-0 left-0 px-5 py-3 bg-gray-100 flex-col flex items-center justify-center">
-            <h1 className="text-3xl md:text-4xl text-center font-semibold pb-3">
-              Marketing Analysice of Maskwell enterprises
-            </h1>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              iusto, molestiae atque explicabo est consequuntur ipsa quasi
-              itaque rem omnis laudantium in saepe culpa esse dolore accusamus
-              soluta ex obcaecati!
-            </p>
-          </div>
+      <div className=" md:flex py-40 gap-20">
+        <div className="basis-2/4 object-cover my-5">
+          <img src={digit} alt="" />
         </div>
-        <div className="relative cursor-pointer port my-10 ">
-          <img src={image} alt="" className="" />
-          <div className="content absolute top-0 bottom-0 right-0 left-0 px-5 py-3 bg-gray-100 flex-col flex items-center justify-center">
-            <h1 className="text-3xl md:text-4xl text-center font-semibold pb-3">
-              Marketing Analysice of Maskwell enterprises
-            </h1>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              iusto, molestiae atque explicabo est consequuntur ipsa quasi
-              itaque rem omnis laudantium in saepe culpa esse dolore accusamus
-              soluta ex obcaecati!
-            </p>
-          </div>
+        <div className="basis-2/4 object-cover my-3">
+          <img src={image_1} alt="" />
         </div>
-        <div className="relative cursor-pointer port">
-          <img src={image} alt="" className="" />
-          <div className="content absolute top-0 bottom-0 right-0 left-0 px-5 py-3 bg-gray-100 flex-col flex items-center justify-center">
-            <h1 className="text-3xl md:text-4xl text-center font-semibold pb-3">
-              Marketing Analysice of Maskwell enterprises
-            </h1>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              iusto, molestiae atque explicabo est consequuntur ipsa quasi
-              itaque rem omnis laudantium in saepe culpa esse dolore accusamus
-              soluta ex obcaecati!
-            </p>
-          </div>
+        <div className="basis-2/4 object-cover my-3">
+          <img src={image_1} alt="" />
         </div>
       </div>
-      <div className="md:flex gap-10  items-center justify-between px-[8%] mb-[100px]">
-        <div className="relative cursor-pointer port">
-          <img src={image} alt="" className="" />
-          <div className="content absolute top-0 bottom-0 right-0 left-0 px-5 py-3 bg-gray-100 flex-col flex items-center justify-center">
-            <h1 className="text-3xl md:text-4xl text-center font-semibold pb-3">
-              Marketing Analysice of Maskwell enterprises
-            </h1>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              iusto, molestiae atque explicabo est consequuntur ipsa quasi
-              itaque rem omnis laudantium in saepe culpa esse dolore accusamus
-              soluta ex obcaecati!
-            </p>
-          </div>
-        </div>
-        <div className="relative cursor-pointer port my-10 ">
-          <img src={image} alt="" className="" />
-          <div className="content absolute top-0 bottom-0 right-0 left-0 px-5 py-3 bg-gray-100 flex-col flex items-center justify-center">
-            <h1 className="text-3xl md:text-4xl text-center font-semibold pb-3">
-              Marketing Analysice of Maskwell enterprises
-            </h1>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              iusto, molestiae atque explicabo est consequuntur ipsa quasi
-              itaque rem omnis laudantium in saepe culpa esse dolore accusamus
-              soluta ex obcaecati!
-            </p>
-          </div>
-        </div>
-        <div className="relative cursor-pointer port">
-          <img src={image} alt="" className="" />
-          <div className="content absolute top-0 bottom-0 right-0 left-0 px-5 py-3 bg-gray-100 flex-col flex items-center justify-center">
-            <h1 className="text-3xl md:text-4xl text-center font-semibold pb-3">
-              Marketing Analysice of Maskwell enterprises
-            </h1>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              iusto, molestiae atque explicabo est consequuntur ipsa quasi
-              itaque rem omnis laudantium in saepe culpa esse dolore accusamus
-              soluta ex obcaecati!
-            </p>
-          </div>
-        </div>
-      </div>
+      {/*  */}
 
       <Contact />
     </div>
